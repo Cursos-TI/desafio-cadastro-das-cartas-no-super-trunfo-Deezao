736230623ffd24463be050bc710bc83f09356ad5
Deezao <andrebesson19@gmail.com>
2025-07-30 03:17:08 +0000

    carta 01 e carta 02

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,27 +2,42 @@
 
 int main(){
 
-    int populacao;  
-    float area;
-    float pib;
-    int pontosturisticos;  
+    int populacao, populacao2;  
+    float area, area2;
+    float pib, pib2;
+    int pontosturisticos, pontosturisticos2;  
 
-    printf("Digite a populacao: \n");
+    printf("Digite a populacao 01: \n");
     scanf("%d", &populacao);
+    printf("Digite a populacao 02: \n");
+    scanf("%d", &populacao2);
 
-    printf("Digite número de pontos turisticos: \n");
+    printf("Digite número de pontos turisticos 01: \n");
     scanf("%d", &pontosturisticos);
+    printf("Digite número de pontos turisticos 02: \n");
+    scanf("%d", &pontosturisticos2);
 
-    printf("Digite o tamanho da area: \n");
+    printf("Digite o tamanho da area 01: \n");
     scanf("%f", &area);
+    printf("Digite o tamanho da area 02: \n");
+    scanf("%f", &area2);
 
-    printf("Digite o PIB: \n");
+    printf("Digite o PIB 01: \n");
     scanf("%f", &pib);
+    printf("Digite o PIB 02: \n");
+    scanf("%f", &pib2);
 
-    printf("Populacao carta n1: %d\n" , populacao);
-    printf("Area: %.2fkm²\n", area);
-    printf("PIB: %f\n", pib);
-    printf("Numero de pontos turisticos: %d\n", pontosturisticos);
+    printf("Carta N1\n");
+    printf("Populacao carta : %d\n",populacao);
+    printf("Area : %.2fkm²\n", area);
+    printf("PIB : %.3f\n", pib);
+    printf("Numero de pontos turisticos : %d\n", pontosturisticos);
+    
+    printf("Carta N2\n");
+    printf("Populacao: %d\n",populacao2);
+    printf("Area: %.2fkm²\n", area2);
+    printf("PIB: %.3f\n", pib2);
+    printf("Numero de pontos turisticos: %d\n", pontosturisticos2);
 
 
     return 0;
