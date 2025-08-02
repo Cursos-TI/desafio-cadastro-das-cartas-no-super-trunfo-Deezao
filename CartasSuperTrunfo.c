ccc02775acb68db95f939b3afb6e899fe8b42eaa
Deezao <andrebesson19@gmail.com>
2025-08-02 01:38:52 +0000

    atualizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,10 +2,22 @@
 
 int main(){
 
+    char estado[25], estado2[25];
+    char nomecidade[25], nomecidade2[25];
     int populacao, populacao2;  
     float area, area2;
     float pib, pib2;
     int pontosturisticos, pontosturisticos2;  
+
+    printf("Digite o nome do estado 01: \n");
+    scanf("%s", &estado);
+    printf("Digite o nome do estado 02: \n");
+    scanf("%s", &estado2);
+    
+    printf("Digite o nome da cidade 01: \n");
+    scanf("%s", &nomecidade);
+    printf("Digite o nome da cidade 02: \n");
+    scanf("%s", &nomecidade2);
 
     printf("Digite a populacao 01: \n");
     scanf("%d", &populacao);
@@ -28,12 +40,16 @@ int main(){
     scanf("%f", &pib2);
 
     printf("Carta N1\n");
+    printf("Estado: %s\n", estado);
+    printf("Nome cidade: %s\n", nomecidade);
     printf("Populacao carta : %d\n",populacao);
     printf("Area : %.2fkm²\n", area);
     printf("PIB : %.3f\n", pib);
     printf("Numero de pontos turisticos : %d\n", pontosturisticos);
     
     printf("Carta N2\n");
+    printf("Estado: %s\n", estado2);
+    printf("Nome cidade: %s\n", nomecidade2);
     printf("Populacao: %d\n",populacao2);
     printf("Area: %.2fkm²\n", area2);
     printf("PIB: %.3f\n", pib2);
